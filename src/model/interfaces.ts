7087f3d329474b26aeab0423d2baf6fd5ac57ef3
hederalves <alvesheder92@gmail.com>
2024-12-09 16:44:04 -0300

    feat: add new interfaces

--- src/model/interfaces.ts
+++ src/model/interfaces.ts
@@ -83,3 +83,54 @@ export interface ThemeContextProps {
     theme: Theme;
     toggleTheme: () => void;
 }
+
+export interface ButtonAdd {
+    href: string;
+}
+
+export interface ArtistCardProps {
+    name: string;
+    country: string;
+    imageUrl: string;
+    onEdit: () => void;
+    onDelete: () => void;
+}
+
+export interface HomeCardProps {
+    title: string;
+    subtitle?: string;
+    songs?: Song[];
+    items: {
+        name: string;
+        amount?: string;
+        [key: string]: string | undefined;
+    }[];
+    theme?: {
+        background?: string;
+        border?: string;
+        title?: string;
+        subtitle?: string;
+        itemText?: string;
+        itemSecondaryText?: string;
+        itemAmount?: string;
+    };
+    link?: string;
+}
+
+export interface ItemCardProps {
+    title: string;
+    description: string;
+    link: string;
+    onClick: () => void;
+    onDelete: () => void;
+}
+
+export interface PlaylistCardProps {
+    title: string;
+    state: string;
+    totalSongs: string;
+    description: string;
+    imageUrl: string;
+    onClick: () => void;
+    onDelete: () => void;
+}
